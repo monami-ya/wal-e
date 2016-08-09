6f293a01080643ca8fe4c7cc5e59dd706d5b70c4
Monami-ya Support <support@monami-ya.com>
2016-08-09 04:57:13 +0000

    Use Python3

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     extras_require={
         'aws': ['boto>=2.40.0'],
         'azure': ['azure>=1.0.3'],
-        'google': ['gcloud>=0.11.0'],
+        'google': ['gcloud>=0.11.0', 'oauth2client'],
         'swift': ['python-swiftclient>=1.8.0',
                   'python-keystoneclient>=0.4.2']
     },
